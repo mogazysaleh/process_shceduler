e34b6971a43db9aeff120dd35fee453441fff627
M7mdSale7 <mogazysaleh@gmail.com>
2021-12-01 17:29:31 -0500

    PCB

--- scheduler.c
+++ scheduler.c
@@ -1,5 +1,9 @@
 #include "headers.h"
 
+enum ProcessState{BLOCKED, READY, RUNNING};
+struct PCB;
+
+
 
 int main(int argc, char * argv[])
 {
@@ -10,3 +14,15 @@ int main(int argc, char * argv[])
     
     destroyClk(true);
 }
+
+struct PCB
+{
+    pid_t processId;
+    enum ProcessState state;
+    int priority;
+    int arrivalTime;
+    int executionTime;
+    int runningTime;
+    int waitingTime;
+    int remainingTime;
+};
